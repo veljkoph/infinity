a7ef368a5f82070b0a888ad8b4deb843c65b28ac
Veljko Ilic <veljkoilik@gmail.com>
2025-01-10 14:17:17 +0100

    user prod permission

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Panel;
+use Filament\Models\Contracts\FilamentUser;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
@@ -48,5 +50,10 @@ class User extends Authenticatable
     public function lang()
     {
         return $this->belongsTo(Language::class);
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
     }
 }
